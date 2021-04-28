69e4b561405ee854cc5fc9ae5189a621e5fcf95a
Prakhar Gupta <gprakhar2000@gmail.com>
2021-04-28 23:48:10 +0530

    updating arrays

--- arrays/Best Time to Buy and Sell Stock.cpp
+++ arrays/Best Time to Buy and Sell Stock.cpp
@@ -1,4 +1,4 @@
-//You are given an array prices where prices[i] is the price of a given stock on the ith day.
+/*You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 
@@ -24,7 +24,7 @@ Constraints:
 1 <= prices.length <= 105
 0 <= prices[i] <= 104//
 
-
+*/
 
 
 class Solution {
